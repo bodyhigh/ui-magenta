6b8ce47ca3655892f47c70ed8cf1fab6964ff43d
Craig Moreno <craig@bodyhigh.com>
2020-04-05 12:47:49 -0700

    Corrected bug when karma testing caused by mat-icon

--- src/app/modules/user-dashboard/user-dashboard/user-dashboard.component.spec.ts
+++ src/app/modules/user-dashboard/user-dashboard/user-dashboard.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { UserDashboardComponent } from './user-dashboard.component';
 
@@ -8,7 +9,8 @@ describe('UserDashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDashboardComponent ]
+      declarations: [ UserDashboardComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
